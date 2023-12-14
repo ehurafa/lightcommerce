da22672af6b5eb05df752644d730dfdbce082057
Rafael Gomes <rafarfl2010@gmail.com>
2023-12-13 22:12:59 -0300

    (template): Get data

--- lightcommerce/src/App.jsx
+++ lightcommerce/src/App.jsx
@@ -1,8 +1,53 @@
+import { useState, useEffect} from "react";
+
+import ProductList from "./components/ProductList";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+import axios from "axios";
+
 function App() {
+
+  const [products, setProducts] = useState([]);
+  const [productDetail, setProductDetail] = useState(null);
+
+  let min = 10;
+  let max = 50;
+
+  let randomPrice = (n) => {
+  return Math.floor(Math.random() * (max - min + n)) + min;
+  }
+
+  const fetchData = async() => {
+
+    const apiKey = import.meta.env.VITE_UNSPLASH_API_KEY;
+
+    const response = await axios.get(`https://api.unsplash.com/photos/random`, {
+      params: {
+        client_id: apiKey,
+        count: 10,
+        query: 'food',
+      }
+    });
+
+
+    const list = [...response.data];
+
+    list.map((product, i) => product.price = randomPrice(i))
+
+    setProducts(list);    
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <>
-      <h1>light commerce</h1>
+      <div className="container">
+        <Header />
+        <ProductList list={ products } />
+        <Footer />
+      </div>
     </>
   )
 }
